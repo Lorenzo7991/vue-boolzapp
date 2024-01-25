eaa85e23faef1bba477165f859ffd7ccaeb1c057
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-01-25 19:20:23 +0100

    add user messages

--- JS/script.js
+++ JS/script.js
@@ -1,24 +1,49 @@
 console.log('VUE-OK', Vue);
 
-const { createApp } = Vue
+const  { createApp } = Vue;
 
-createApp({
+const app = createApp({
     data() {
         return {
             ...data,
             selectedContact: '',
+            newMessage: '',
+            chatMessages: [],
+            nextMessageId: '',
+        };
+    },
+    computed: {
+        highestIdMessage() {
+            //* Computes a message based on whether a contact is selected or not
+            return this.selectedContact ? 'Type a message' : 'Select a contact to start chatting...';
+        },
+        placeholderText() {
+            //* Computes placeholder text based on whether a contact is selected or not
+            return this.selectedContact ? 'Type a message' : 'Select a contact to start chatting...';
         }
     },
     methods: {
-        selectContact(contact)  {
+        selectContact(contact) {
+            //* Method to select a contact
             this.selectedContact = contact;
-        } ,
+        },
         getMessageClasses(message) {
-            return {
-                'message': true,
-                'sent': message.status === 'sent',
-                'received': message.status === 'received'
-            };
+            //* Method to compute classes for styling messages
+            return {'message': true, 'sent': message.status === 'sent', 'received': message.status === 'received'};
+        },
+        sendMessage() {
+            //* Computes the ID for the new message
+            if (this.newMessage.trim()) {
+                const newMessageId = this.highestIdMessage + 1;
+                //*New message obj
+                const newMessage = { id: newMessageId, text: this.newMessage.trim(), status: 'sent'};
+                //* Adds the new message to the chat messages
+                this.chatMessages.push(newMessage);
+                //* Resets the new message input field
+                this.newMessage = '';
+            }
         }
     }
-}).mount('#root')
+})
+app.mount('#root');
+
